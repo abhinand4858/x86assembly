947c68701a2ab3d14e045727fc20fd4d746581b1
Abhinand N <01abhinandn@gmail.com>
2017-09-18 15:15:39 +0530

    Implemented sum of two numbers

---
+++ noSyscall/2_sum_of_two_numbers.asm
@@ -0,0 +1,49 @@
+extern printf
+extern scanf
+
+section .data
+	inp: db "%d"
+	out: db "Sum is %d"
+	print: db "Enter the number:"
+	x: dd 0
+	y: dd 0
+
+
+section .text
+	global main
+
+	main:
+		push ebp
+		mov ebp, esp 
+
+		push print 
+		call printf
+		add esp, 4
+
+		push x
+		push inp 
+		call scanf 
+		add esp, 8
+
+		push print 
+		call printf
+		add esp, 4
+
+		push y
+		push inp
+		call scanf 
+		add esp, 8
+
+		mov eax, dword [x]
+		add eax, dword [y]
+
+		push eax
+		push out
+		call printf 
+		add esp, 8
+
+		mov eax, 0
+		leave 
+		ret
+		
+
